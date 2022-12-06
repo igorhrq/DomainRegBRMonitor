0c99460bfcb7400c34d1b6c66f2db8efb558ecf5
Igor Andrade <igorhenriquehdbr@gmail.com>
2022-12-06 12:29:44 -0300

    Update DomainRegBR-Monitor.sh

--- DomainRegBR-Monitor.sh
+++ DomainRegBR-Monitor.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
-# Igor Andrade on 15/06/2022 
-# Debian-PB.org
+# Author: Igor Andrade 
+# First Release in 15/06/2022 
+# Thanks to Debian-PB.org
 #
-# Revamped on 01/12/2022
+# Rescrito em 01/12/2022
+#  - Adicionado Telegram
+#  - Adicionado Checks de validação binários
+#  - Adicionado Validação de teste do whois em caso de timeout
+#  - Melhoria de código
+#  - Adicionado Comentários 
+#
+# Script para monitorar status de domínios do registro.br
 # Script to monitor domains avaibility to purchase on registro.br
 #
-# This script should send everyday data about the status of the domain, when they enter on the lists of registro.br you will see the status as available to
-# enter in a dispute to register it.
+#
+# Script para monitoramento de domínios do registro.br apenas, auxiliará durante o processo de disputa, e avisará quando o mesmo estiver disponível para submeter processo de disputa dele
+# entrando nas listas de competitividade do registro.br
 
 
 
-#constant Vars
+# Variáveis constantes
 
 #@ webhooks @#
 
-# Choice webhook to use
-# Only Telegram = 1
-# Only Teams = 2 
-# Use Both Telegram and Teams set = 3
+# Escolha uma opção para uso de webhooks
+# Apenas Telegram = 1
+# Apenas Teams = 2 
+# Usar ambos Telegram e Teams ajuste = 3
 ChoiceWebHook="0"
 
-#teams
-URL_TEAMS1="https://yourteamsURL.com/webhookb2/IncomingWebhook/....."
+# Dados teams webhook (dados abaixo são inválidos, e precisam ser substituidos pelos seus)
+URL_TEAMS1="https://suaURLdoteams.com/webhookb2/IncomingWebhook/....."
 
-#telegram
+# Dados telegram webhook (dados abaixo são inválidos, e precisam ser substituidos pelos seus)
 chat_id="-1000000000"
 token="1000000000:AAAAA5AlaaAS8q19HGFJjiAAAAAzzzTwOyxc"
 URLTelegram="https://api.telegram.org/bot$token/sendMessage"
 
 
-# Do a list about the domains that you want monitor 
+# Escreva uma lista de domínios a serem monitorados
 domainz="
 DomainToMonitor.com.br
 Domain2ToMonitor.com.br
@@ -37,15 +46,17 @@ Domain3ToMonitor.com.br
 "
 
 
-# DO NOT CHANGE ANYTHING BELOW THIS LINE
+# NAO ALTERAR NADA NO CÓDIGO APOS ESSA LINHA
 ###################################################################
 
-# Check domain on lists if released for registration on Registro-BR:
+# Checa as listas do registro.br e verifica se os domínios escolhidos estão em alguma delas
 lista1="https://registro.br/dominio/processo-de-liberacao/"
 lista2="https://registro.br/dominio/lista-processo-competitivo.txt"
 lista3="https://registro.br/dominio/lista-competicao.txt"
 testOnly="registro.br"
 
+
+# Função que dispara os webhooks
 send_alarm()
 {
 
@@ -59,6 +70,7 @@ send_alarm()
         curl -d '{"@type": "MessageCard","@context": "http://schema.org/extensions","themeColor": "0076D7","summary": "DomainRegBRAlerts :: Domain monitor","sections": [{"activityTitle": "DomainRegBRAlerts :: Domain '"$Dominios"' monitor","activitySubtitle": "","activityImage": "https://mir-s3-cdn-cf.behance.net/projects/404/62c830116280499.Y3JvcCwxNjcyLDEzMDcsNDE4LDMyMg.png","facts": [{"name": "List Liberaçao:","value": "'"$liberacao"'"}, {"name": "List Competitividade:","value": "'"$competitividade"'"}, {"name": "List Competicao:","value": "'"$competicao"'"}, {"name": "Status on whois is now:","value": "'"$status"'"}, {"name": "Last change on registrar is:","value": "'"$lastchange"'"}, {"name": "Date today:","value": "'"$(date "+%d-%m-%Y %H:%M:%S")"'"}, {"name": "Lists to Check:","value": "ListaLiberacao: '"$lista1"' ListaCompetitividade: '"$lista2"' ListaCompeticao: '"$lista3"'"}],"markdown": true}], "potentialAction": [{"@type": "OpenUri","name": "Open Documentations and List","targets": [{"os": "default","uri": "'"$URLregistrobr"'"}]}]}' -H 'Content-Type: application/json; charset=UTF-8' "$URL_TEAMS1"
     }
 
+# Analisa váriavel de escolha de webhooks
     if [ $ChoiceWebHook -eq "1" ]
         then
         telegram
@@ -70,10 +82,11 @@ send_alarm()
         teams
         telegram
     else
-        echo -e "Wrong Option choiced on variable ChoiceWebHook, Please set 1,2 or 3 for this variable"
+        echo -e "Opção errada escolhida na váriavel ChoiceWebHook, Por favor escolha o número 1,2 or 3 para essa variável, detalhes nos comentários do código"
     fi
 }
 
+# Lista de liberação do registro.br
 processoLiberacao(){
 curl -ks https://registro.br/dominio/lista-processo-liberacao.txt | egrep "^$Dominios$"
 
@@ -84,6 +97,8 @@ curl -ks https://registro.br/dominio/lista-processo-liberacao.txt | egrep "^$Dom
 	fi
 }
 
+# Lista Competitivo do registro.br
+
 processoCompetitivo(){
 curl -ks https://registro.br/dominio/lista-processo-competitivo.txt | egrep "^$Dominios$"
 
@@ -93,6 +108,8 @@ curl -ks https://registro.br/dominio/lista-processo-competitivo.txt | egrep "^$D
 		competitividade=$(echo -e "Not yet on list Competitividade... Monitoring")
 	fi
 }
+
+# Lista Competição do registro.br
 
 processoCompeticao(){
 curl -ks https://registro.br/dominio/lista-competicao.txt | egrep "^$Dominios$"
@@ -105,6 +122,7 @@ curl -ks https://registro.br/dominio/lista-competicao.txt | egrep "^$Dominios$"
 }
 
 
+# Função principal, verifica o domínio passado 1 por 1 dentro de um loop(for) através do whois, checa o status e checa a última alteração
 
 main(){
 for Dominios in $domainz 
@@ -119,7 +137,7 @@ for Dominios in $domainz
         lastchange="This domain should be available or very close"
     fi
 
-    #Load other functions
+    # Carrega funções complementares
     processoLiberacao
     processoCompetitivo
     processoCompeticao
@@ -127,6 +145,7 @@ for Dominios in $domainz
 done
 }
 
+# Checks inicais antes do script rodar, analisa se os binários existem no servidor do whois e curl existem e checa se o whois dá timeout por algum motivo
 if [ -f /usr/bin/whois ] && [ -f /usr/bin/curl ]
     then
             if /usr/bin/whois $testOnly >/dev/null 2>&1
